16c406ca5ef5eda8d6f818ac14e6d2713ec3f119
SamsClems <sam-hearts@hotmail.com>
2021-11-17 20:23:21 -0700

    add html templates

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ from sqlalchemy.sql.expression import extract
 from sqlalchemy.sql.schema import Index
 
 from config import password
-connection_string = f"postgres:{password}@localhost:5432/Tornado"
-engine = create_engine(f'postgresql://{connection_string}')
-# engine = create_engine("postgresql://postgres:postgres@localhost:5432/Tornado")
+# connection_string = f"postgres:{password}@localhost:5432/Tornado"
+# engine = create_engine(f'postgresql://{connection_string}')
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/Tornado")
 
 # reflect an existing database into a new model
 Base = automap_base()
